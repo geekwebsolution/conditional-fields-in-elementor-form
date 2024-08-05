a4f372f2ceef509dbd6b7dbb46149ab2a1f165a9
geek-samarth <samarthgeek435@gmail.com>
2024-08-05 18:13:48 +0530

    v1.0.0

---
+++ includes/class-conditional-fields-in-elementor-form-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://geekcodelab.com/
+ * @since      1.0.0
+ *
+ * @package    Conditional_Fields_In_Elementor_Form
+ * @subpackage Conditional_Fields_In_Elementor_Form/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Conditional_Fields_In_Elementor_Form
+ * @subpackage Conditional_Fields_In_Elementor_Form/includes
+ * @author     Geek Code Lab <support@geekcodelab.com>
+ */
+class Conditional_Fields_In_Elementor_Form_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-conditional-fields-in-elementor-form-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://geekcodelab.com/
+ * @since      1.0.0
+ *
+ * @package    Conditional_Fields_In_Elementor_Form
+ * @subpackage Conditional_Fields_In_Elementor_Form/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Conditional_Fields_In_Elementor_Form
+ * @subpackage Conditional_Fields_In_Elementor_Form/includes
+ * @author     Geek Code Lab <support@geekcodelab.com>
+ */
+class Conditional_Fields_In_Elementor_Form_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
